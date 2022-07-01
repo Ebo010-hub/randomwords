5777f08368d6614de6d034c65bd645288b271488
Retsiela <retsiela@users.noreply.github.com>
2022-07-01 16:10:38 +0200

    Update Pythoncodepickrandom.py

--- Pythoncodepickrandom.py
+++ Pythoncodepickrandom.py
@@ -1,18 +1,22 @@
+#!/bin/python3
+
 # Python code to pick a random
 # word from a text file
 import random
 
-i = 1
-while i < 500:
-      with open("./MyFile.txt", "r") as file:
+
+def randonmwords():
+    with open("./MyFile.txt", "r") as file:
         allText = file.read()
         words = list(map(str, allText.split()))
+    
+    i = 1
+    while i < len(words):
+        words = list(map(str, allText.split()))
         wordlist = random.choice(words)
-        wordlist1 = random.choice(words)
-        wordlist2 = random.choice(words)
-        wordlist3 = random.choice(words)
-        wordlist4 = random.choice(words)
-        wordlist5 = random.choice(words)
-        wordlist6 = random.choice(words)
-       print (wordlist+' '+wordlist1+' '+wordlist2+' '+wordlist3+'                              '+wordlist4+' '+wordlist5+' '+wordlist6)
-       i += 1
+        print(wordlist, end=" ") 
+        i += 1
+
+
+if __name__ == '__main__':
+   randonmwords()
